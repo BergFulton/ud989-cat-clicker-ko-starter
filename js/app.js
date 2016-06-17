fb9b8cfc304bae822700bd7cb787046cace94c2e
BergFulton <BergFulton@users.noreply.github.com>
2016-06-16 22:59:13 -0600

    wip: unexpected end error fixing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-var ViewModel = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/basset_hound.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
+
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames)
+	
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 5) {
 			return "Newborn";
@@ -13,12 +16,25 @@ var ViewModel = function(){
 			return "Baby";
 		} else if (this.clickCount() < 100){
 			return "Teen" ;
+		} else if (this.clickcount()< 150){
+			return "Adult";
 		}
-	}, this)
+	}, this);
+	
+
+var ViewModel = function(){
+	var self = this;
+	this.currentCat = ko.observable( new Cat({
+	clickCount: 0,
+	name: 'Tabby',
+	imgSrc: 'img/basset_hound.jpg',
+	imgAttribution: 'https://www.flickr.com/photos/big',
+	nicknames: ['Blorps', 'Chubs', 'Bunda'],
+	}) );
 	
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
-}
+}	
 
 ko.applyBindings(new ViewModel());
